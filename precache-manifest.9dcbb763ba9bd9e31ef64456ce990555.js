0f63f9fc1ec48a73670ea0bcab0beb98787b048b
Code Sandbox <hello@codesandbox.io>
2022-01-30 01:07:30 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.9dcbb763ba9bd9e31ef64456ce990555.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f0c2ca63108f62b1f964ccf2f8ded85",
+    "url": "https://manarea.github.io/csb-ljs9c/index.html"
+  },
+  {
+    "revision": "d94700a392a20824db98",
+    "url": "https://manarea.github.io/csb-ljs9c/static/css/2.f37fde72.chunk.css"
+  },
+  {
+    "revision": "962f11135cb8f6824475",
+    "url": "https://manarea.github.io/csb-ljs9c/static/css/main.f59633e8.chunk.css"
+  },
+  {
+    "revision": "d94700a392a20824db98",
+    "url": "https://manarea.github.io/csb-ljs9c/static/js/2.a5ec4c80.chunk.js"
+  },
+  {
+    "revision": "962f11135cb8f6824475",
+    "url": "https://manarea.github.io/csb-ljs9c/static/js/main.bff2cf58.chunk.js"
+  },
+  {
+    "revision": "d6a32f530400df2ef81a",
+    "url": "https://manarea.github.io/csb-ljs9c/static/js/runtime~main.e2892ed0.js"
+  }
+]);
